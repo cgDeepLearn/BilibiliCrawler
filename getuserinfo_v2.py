fafb89e637be6263b5503d58df56cdbcb7e3ca2b
cgDeepLearn <xiujiaw@gmail.com>
2017-11-16 13:43:52 +0800

    start version 2.0

---
+++ getuserinfo_v2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf8 -*-
+"""
+get bilibili user info v2
+change url to api.bilibili
+"""
+
+
+# http://api.bilibili.com/cardrich?callback=jQuery17202870352235622704_1482889079913&mid=122541&type=jsonp&_=1482891272353
+
