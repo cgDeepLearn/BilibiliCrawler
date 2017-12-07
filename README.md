523e397d1323008bc521d2321ea694289415283d
cgDeepLearn <cglearningnow@163.com>
2017-12-07 16:10:02 +0800

    fix initial

--- README.md
+++ README.md
@@ -3,8 +3,17 @@
 - 爬取哔哩哔哩up主信息和up主投稿视频信息，用作数据处理
 - 采取了一定的反反爬策略
 
+## 快速开始
+1. 拉取项目, git clone https://github.com/cgDeepLearn/BilibiliCrawler.git
+2. 进入项目主目录，安装虚拟环境crawlenv(请参考使用说明里的虚拟环境安装)
+3. 激活环境并在主目录运行crawl,爬取结果将保存在data目录csv文件中
+```python
+source activate crawlenv
+python crawl_user.py file 1 100  # file是数据保存方式，1 100是开始、结束bilibili的uid
+```
+
 ## 使用说明
-1. git clone 拉取项目
+1. 拉取项目, git clone https://github.com/cgDeepLearn/BilibiliCrawler.git
 
 2. 进入项目主目录， 安装虚拟环境
 - 若已安装anaconda
--- initial.py
+++ initial.py
@@ -2,8 +2,23 @@
 """
 数据库初始化
 """
+import os
+import sys
 from db import Base, eng
+from config import BASE_DIR
 
-Base.metadata.drop_all(eng)  # 删除表
-Base.metadata.create_all(eng)  # 建立表
-print("done!")
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("run with 1 arg : db or file to choose the mode while initializing")
+        exit(-1)
+    mode = sys.argv[1]
+    if mode == 'file':
+        data_dir = os.path.join(BASE_DIR, 'data')
+        if not os.path.exists(data_dir):
+            os.mkdir(data_dir)
+    elif mode == 'db':
+        # Base.metadata.drop_all(eng)  # 删除表
+        Base.metadata.create_all(eng)  # 建立表
+        print("done!")
+    else:
+        print("wrong mode, run by use arg db or file!")
